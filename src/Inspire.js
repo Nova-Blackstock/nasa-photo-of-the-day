791d465b8fdd0df2aacf488d3972b18c1bb7b9fc
Nova Blackstock <fractalrefletions@gmail.com>
2020-07-16 15:57:38 -0700

    Big NASA improvements

--- src/Inspire.js
+++ src/Inspire.js
@@ -1,17 +1,104 @@
 import React from 'react'
+import style, {keyframes} from 'styled-components'
+
+const kfquote = keyframes`
+    100% {
+        color: black;
+    }
+`
+const kfinspire = keyframes`
+    50% {
+        background: lavender;
+    }
+    100% {
+        background: black;
+    }
+`
+
+const kfheading = keyframes`
+    25%{
+        border: 3px solid red;
+    }
+    80%{
+        border: 1px solid maroon;
+    }
+    100% {
+        background-color: maroon;
+        border: 50px solid maroon;
+        color: orange;
+        box-shadow: -10px -10px 80px 5px;
+    }
+`
+
+const StyledInspire = style.div`
+    display: flex;
+    justify-content: space-evenly;
+    background: orange;
+    animation: ${kfinspire} 5s ease-in-out forwards;
+`
+
+const StyledVenus = style.div`
+    border-radius: 10%;
+    img {
+        margin: 5%;
+        border-radius: 50% 50% 50% 50%;
+        color: blue;
+        box-shadow: -5px -5px 50px 10px;
+        border: 2px solid aqua;
+    }
+    margin: 5%;
+    
+`
+
+const StyledHeading = style.div`
+    background-color: red;
+    animation: ${kfheading} 10s ease-in-out forwards;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 25%;
+    margin: 5%;
+    border-radius: 50% 50% 50% 50%;
+    box-shadow: -10px -10px 80px 80px;
+    border: 3px solid yellow;
+    h1 {
+        color: red;
+        animation: ${kfquote} 2s ease-in-out forwards;
+    }
+    h2 {
+        background: black;
+        color: purple;
+        font-size: 1.5rem;
+        padding: 10px;
+        width: 15%;
+        box-shadow: -10px -10px 80px 20px;
+        border-radius: 50% 50% 50% 50%;
+        border: 1px solid lavender;
+        &:hover {
+            background: gold;
+            color: white;
+        }
+    }
+`
 
 function Inspire(props){
     const {nasa} = props
     console.log('Inspiration is here')
 
     return(
-        <div className= "Inspiration Quote">
+        <StyledInspire>
+        <StyledHeading className= "Inspiration Quote">
             <h1 className= "Inspiration Quote">
-                "The history of astronomy is a history of receding horizons" - Edwin Hubble
+            "There is geometry in the humming of the strings, there is music in the spacing of the spheres" - Pythagorus
             </h1>  
             <span role="img" aria-label='go!'>🚀</span>
-            <h2>In 200,000 years it will be 20{nasa.date}</h2>
-        </div>         
+            <h2>{nasa.date}</h2>
+        </StyledHeading>
+        <StyledVenus className= 'Venus'>
+            <img src='https://www.sciencealert.com/images/2019-07/ysaLqAf1.gif' alt='Earth and Venus'></img>
+        </StyledVenus>
+        </StyledInspire>         
     )
 }
 
